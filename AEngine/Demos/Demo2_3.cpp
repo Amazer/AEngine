250c0450db3de8f8e3a686aeb8ad7ea95be9ef7b
Amazer <149779511@qq.com>
2017-09-02 19:43:46 +0800

    demo2_3

--- AEngine/Demos/Demo2_3.cpp
+++ AEngine/Demos/Demo2_3.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define  WINDOW_CLASS_NAME="WINCLASS1"
+#define  WINDOW_CLASS_NAME "WINCLASS1"
 
 // 消息回调方法
 LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
@@ -15,7 +15,89 @@ LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 // 所有Windows程序的入口
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	WNDCLASSEX winclass;
+	HWND hwnd;
+	MSG msg;
+
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC | CS_DBLCLKS;
+	winclass.lpfnWndProc = WinProc;
+	winclass.cbClsExtra = 0;
+	winclass.cbWndExtra = 0;
+	winclass.hInstance = hInstance;
+	winclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+//	winclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	winclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	winclass.lpszClassName = WINDOW_CLASS_NAME;
+	winclass.lpszMenuName = NULL;
+	winclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&winclass))
+	{
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(NULL,
+		WINDOW_CLASS_NAME,
+		"Demo2_3",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,				// window's style
+		0, 0,
+		1280, 720,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hwnd)
+	{
+		return 0;
+	}
 
 
-	return 0;
+	ShowWindow(hwnd, nShowCmd);
+
+	UpdateWindow(hwnd);			//将一条WM_PAINT发送给窗口，以刷新其内容
+
+	while (GetMessage(&msg,NULL,0,0))
+	{
+		TranslateMessage(&msg);		//加速转换消息
+
+		DispatchMessage(&msg);		//分发消息
+	}
+
+	return msg.wParam;
 }
+LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	PAINTSTRUCT ps;		// 用于paint的数据结构
+	HDC hdc;			// 设备场景句柄
+	switch (msg)
+	{
+	case WM_CREATE:				//Create
+	{
+		// 执行初始化代码
+
+		return 0;
+	}
+	break;
+	case WM_PAINT:				//paint
+	{
+
+		hdc = BeginPaint(hwnd, &ps);
+
+		EndPaint(hwnd, &ps);
+		return 0;
+	}
+	break;
+	case WM_DESTROY:			//destory
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	break;
+	}
+
+	return (DefWindowProc(hwnd, msg, wparam, lparam));
+}		//end WinProc
